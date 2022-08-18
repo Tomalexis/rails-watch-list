712224d82bc75935a7361fa2aa2c91dbb1df5b21
Thomas Girard <thomasalexisgirard@gmail.com>
2022-08-18 18:14:43 +0200

    work in progress

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,23 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.save
+    redirect_to list_path(@bookmark.list)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+wonder = Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+shosho = Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+titanic = Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+ocean = Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+
+list_1 = List.create(name: 'First list')
+Bookmark.create(comment: 'Au secours', movie: titanic, list: list_1)
+Bookmark.create(comment: 'Mamma Mia', movie: ocean, list: list_1)
+list_2 = List.create(name: 'Nullos list')
+Bookmark.create(comment: 'Très mauvais', movie: shosho, list: list_2)
+Bookmark.create(comment: 'Pas bien', movie: wonder, list: list_2)
+puts "#{Movie.all.count} movies"
+puts "#{List.all.count} lists"
+puts "#{Bookmark.all.count} bookmarks"
